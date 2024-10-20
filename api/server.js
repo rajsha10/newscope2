3ac6d215b278b67aee8177d9830a752e29a8896e
rajsha10 <rajsha8617@gmail.com>
2024-10-20 12:29:17 +0530

    server.js error dixed

--- api/server.js
+++ api/server.js
@@ -6,15 +6,13 @@ import newsRouter from './routes/news.routes.js';
 import userRouter from './routes/user.routes.js';
 import adminRouter from './routes/admin.routes.js';
 
-
-
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
 
 const corsOptions = {
-     origin: 'http://localhost:5173', // Replace with your frontend's URL
+     origin: 'https://newscope2.vercel.app/', // Replace with your frontend's URL
      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
      credentials: true, // Allow credentials (e.g., cookies) to be sent
 };
